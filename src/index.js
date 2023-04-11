69e1b557b08320e32b0d8e292458d8e0f43b073e
Molly Donegan <mollyrdonegan@gmail.com>
2023-04-11 16:40:54 -0700

    UI WIP

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import "./css/styles.css";
 import * as plant from './js/plant.js';
 
 
+let olivia = plant.orchid("Olivia");
+const blooming = plant.canBloom(olivia);
+const shading = plant.canCreateShade(olivia);
+document.getElementById('blooming-orchid').innerText = `Blooming: ${blooming.color("purple")}`;
+document.getElementById('shading-orchid').innerText = `Creating shade: ${shading.shadeSize("little")}`;
+
+let petrie = plant.palm("Petrie");
+const shadingPalm = plant.canCreateShade(petrie);
+document.getElementById('shading-palm').innerText = `Creating shade: ${shadingPalm.shadeSize("medium")}`;
+
+let avery = plant.aloe("Avery");
+const healing = plant.canHeal(avery);
+document.getElementById('healing-aloe').innerText = `Healing: ${healing.healingCapabilities("cuts")}`;
+
 //User Interface Logic
 window.onload = function() {
 
@@ -11,24 +25,74 @@ window.onload = function() {
   // Manipulating the DOM will always be a side effect. 
   // Note that we only use one of our functions to alter soil. 
   // You can easily add more.
-  document.getElementById('feed').onclick = function() {
-    const newState = plant.stateControl(plant.blueFood);
-    document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
+
+  // document.getElementById('feed').onclick = function() {
+  //   const newState = oliviaStateControl(plant.blueFood);
+  //   document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
+  // };
+
+  // document.getElementById('feedLittle').onclick = function() {
+  //   const newState = plant.stateControl(plant.feed);
+  //   document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
+  // };
+
+  // document.getElementById('hydrate').onclick = function() {
+  //   const newState = plant.stateControl(plant.hydrate);
+  //   document.getElementById('water-value').innerText = `Water: ${newState.water}`;
+  // };
+  // document.getElementById('superWater').onclick = function() {
+  //   const newState = plant.stateControl(plant.superWater);
+  //   document.getElementById('water-value').innerText = `Water: ${newState.water}`;
+  // };
+
+  document.getElementById('feed-orchid').onclick = function() {
+    const newState = plant.stateControlOrchid(plant.feed);
+    document.getElementById('soil-value-orchid').innerText = `Soil: ${newState.soil}`;
   };
 
-  document.getElementById('feedLittle').onclick = function() {
-    const newState = plant.stateControl(plant.feed);
-    document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
+  document.getElementById('water-orchid').onclick = function() {
+    const newState = plant.stateControlOrchid(plant.hydrate);
+    document.getElementById('water-value-orchid').innerText = `Water: ${newState.water}`;
   };
 
-  document.getElementById('hydrate').onclick = function() {
-    const newState = plant.stateControl(plant.hydrate);
-    document.getElementById('water-value').innerText = `Water: ${newState.water}`;
+  document.getElementById('light-orchid').onclick = function() {
+    const newState = plant.stateControlOrchid(plant.light);
+    document.getElementById('light-value-orchid').innerText = `Light: ${newState.light}`;
   };
-  document.getElementById('superWater').onclick = function() {
-    const newState = plant.stateControl(plant.superWater);
-    document.getElementById('water-value').innerText = `Water: ${newState.water}`;
+
+  document.getElementById('feed-palm').onclick = function() {
+    const newState1 = plant.stateControlPalm(plant.feed);
+    document.getElementById('soil-value-palm').innerText = `Soil: ${newState1.soil}`;
   };
+
+  document.getElementById('water-palm').onclick = function() {
+    const newState1 = plant.stateControlPalm(plant.hydrate);
+    document.getElementById('water-value-palm').innerText = `Water: ${newState1.water}`;
+  };
+
+  document.getElementById('light-palm').onclick = function() {
+    const newState1 = plant.stateControlPalm(plant.light);
+    document.getElementById('light-value-palm').innerText = `Light: ${newState1.light}`;
+  };
+
+  document.getElementById('feed-aloe').onclick = function() {
+    const newState2 = plant.stateControlAloe(plant.feed);
+    document.getElementById('soil-value-aloe').innerText = `Soil: ${newState2.soil}`;
+  };
+
+  document.getElementById('water-aloe').onclick = function() {
+    const newState2 = plant.stateControlAloe(plant.hydrate);
+    document.getElementById('water-value-aloe').innerText = `Water: ${newState2.water}`;
+  };
+
+  document.getElementById('light-aloe').onclick = function() {
+    const newState2 = plant.stateControlAloe(plant.light);
+    document.getElementById('light-value-aloe').innerText = `Light: ${newState2.light}`;
+  };
+  
+
+
+
 
   // This function doesn't actually do anything useful in this application 
   // — it just demonstrates how we can "look" at the current state 
--- src/js/plant.js
+++ src/js/plant.js
@@ -9,7 +9,9 @@ export const storeState = () => {
   };
 };
 
-export const stateControl = storeState();
+export const stateControlOrchid = storeState();
+export const stateControlPalm = storeState();
+export const stateControlAloe = storeState();
 
 // This is a function factory. 
 // We can easily create more specific functions that 
@@ -30,6 +32,7 @@ export const blueFood = changeState("soil")(5);
 
 export const hydrate = changeState("water")(1);
 export const superWater = changeState("water")(5);
+export const light = changeState("light")(1);
 
 // Plant Method Compositions
 export const canBloom = (plant) => ({
@@ -41,7 +44,7 @@ export const canCreateShade = (plant) => ({
 });
 
 export const canHeal = (plant) => ({
-  healingCapabilities: (healingCapabilities) => `${plant.name} ican heal ${healingCapabilities}.`
+  healingCapabilities: (healingCapabilities) => `${plant.name} can heal ${healingCapabilities}.`
 });
 
 // Plant Function Factories
