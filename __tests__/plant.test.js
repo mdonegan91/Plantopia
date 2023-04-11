ba99cbfb31eb00cf93e576417a8eefd3a4f26e3c
Molly Donegan <mollyrdonegan@gmail.com>
2023-04-11 13:53:06 -0700

    pass storeState test

--- __tests__/plant.test.js
+++ __tests__/plant.test.js
@@ -1,5 +1,15 @@
 import * as plant from './../src/js/plant.js';
 
+describe('plant functions', () => {
+
+  describe('storeState', () => {
+    test('should create a state store that can store and retrieve state', () => {
+      const stateControl = plant.storeState();
+      const newState = stateControl(plant.changeState('soil')(1));
+      expect(newState).toEqual({ soil: 1 });
+    });
+  });
+
 describe('feed', () => {
 
   test("Should increase soil level by 1", () => {
@@ -7,4 +17,6 @@ describe('feed', () => {
     soil = plant.feed(2);
     expect(soil).toEqual({"soil": 1});
   });
+});
+
 });
